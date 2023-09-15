544e7881d9ea8029b7c379abb3859117e48e8e9a
BelloDev <fernandojbf123@gmail.com>
2023-09-14 18:24:30 -0700

    almost done main

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,76 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import {useState, useEffect, useRef} from "react"
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const canvasRef = useRef(null);
+  const ctxRef = useRef(null);
+  const [isMouseDown, setIsMouseDown] = useState(false)
+
+
+  function handleDown (e) {
+    const x = e.nativeEvent.offsetX;
+    const y = e.nativeEvent.offsetY;
+    ctxRef.current.beginPath();
+    ctxRef.current.moveTo(x,y)
+    ctxRef.current.lineTo(x,y)
+    ctxRef.current.stroke();
+    setIsMouseDown(true)
+  }
+
+  function handleMove(e) {
+    if (!isMouseDown) {
+      return
+    }
+
+    const x = e.nativeEvent.offsetX;
+    const y = e.nativeEvent.offsetY;
+    ctxRef.current.moveTo(x,y)
+    ctxRef.current.lineTo(x,y)
+    ctxRef.current.stroke();
+  }
+
+  function handleUp () {
+    ctxRef.current.closePath();
+    setIsMouseDown(false)
+  }
+
+
+  useEffect ( ()=> {
+    
+    //set canvas properties
+    const canvas = canvasRef.current
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+    canvas.style.width = `${window.innerWidth}px`;
+    canvas.style.height = `${window.innerHeight}px`;
+    
+    //create context
+    const ctx = canvas.getContext("2d");
+    ctx.lineCap="round"
+    ctx.strokeStyle="blue"
+    ctx.lineWidth = 5;
+    ctxRef.current = ctx;
+
+
+  },[])
+
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div id="container">
+
+        <aside id="bar">
+          <meter value={5} min={1} max={20}></meter>
+
+        </aside>
+
+        <canvas id="myCanvas" 
+          ref={canvasRef}
+          onMouseDown={handleDown}
+          onMouseMove={handleMove}
+          onMouseUp={handleUp}
+        ></canvas>
+
+    </div>
   )
 }
 
